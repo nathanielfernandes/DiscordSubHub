fe62b79bdc1602d68f7b5f2c196ab2834054932c
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2021-03-01 17:32:33 -0500

    removed main

--- discordsubhub.py
+++ discordsubhub.py
@@ -77,6 +77,6 @@ async def web_app():
     return app
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    web.run_app(web_app(), port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
+#     web.run_app(web_app(), port=port)
